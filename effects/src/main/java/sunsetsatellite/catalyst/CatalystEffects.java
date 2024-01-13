61ee6e45c179c8b08377de531bb9ac3ef77da609
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2024-01-13 12:19:34 +0100

    Client-side logic now in separate file.

--- effects/src/main/java/sunsetsatellite/catalyst/CatalystEffects.java
+++ effects/src/main/java/sunsetsatellite/catalyst/CatalystEffects.java
@@ -1,30 +1,15 @@
 package sunsetsatellite.catalyst;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.options.components.BooleanOptionComponent;
-import net.minecraft.client.gui.options.components.KeyBindingComponent;
-import net.minecraft.client.gui.options.components.OptionsCategory;
-import net.minecraft.client.gui.options.components.ToggleableOptionComponent;
-import net.minecraft.client.gui.options.data.OptionsPage;
-import net.minecraft.client.gui.options.data.OptionsPages;
 import net.minecraft.core.data.registry.Registries;
-import net.minecraft.core.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sunsetsatellite.catalyst.effects.ItemGiveEffect;
 import sunsetsatellite.catalyst.effects.api.attribute.Attributes;
-import sunsetsatellite.catalyst.effects.api.effect.Effect;
-import sunsetsatellite.catalyst.effects.api.effect.EffectDisplayPlace;
 import sunsetsatellite.catalyst.effects.api.effect.Effects;
-import sunsetsatellite.catalyst.effects.api.modifier.Modifier;
-import sunsetsatellite.catalyst.effects.api.modifier.ModifierType;
-import sunsetsatellite.catalyst.effects.api.modifier.type.IntModifier;
 import sunsetsatellite.catalyst.effects.command.AttributesCommand;
 import sunsetsatellite.catalyst.effects.command.EffectsCommand;
 import sunsetsatellite.catalyst.effects.interfaces.mixins.IKeybinds;
 import turniplabs.halplibe.helper.CommandHelper;
-import turniplabs.halplibe.helper.ItemHelper;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.TomlConfigHandler;
 import turniplabs.halplibe.util.toml.Toml;
@@ -54,11 +39,6 @@ public class CatalystEffects implements ModInitializer, GameStartEntrypoint {
 
 	@Override
 	public void afterGameStart() {
-		keybinds = ((IKeybinds) Minecraft.getMinecraft(Minecraft.class).gameSettings);
-		OptionsPage optionsPage = new OptionsPage("gui.options.page.catalyst-effect");
-		optionsPage
-			.withComponent(new ToggleableOptionComponent<>(keybinds.getEffectDisplayPlaceEnumOption()));
-		OptionsPages.register(optionsPage);
 		Registries.getInstance().register("catalyst:effects",Effects.getInstance());
 		Registries.getInstance().register("catalyst:attributes",Attributes.getInstance());
 		LOGGER.info(String.format("%d attributes registered.",Attributes.getInstance().size()));
---
+++ effects/src/main/java/sunsetsatellite/catalyst/CatalystEffectsClient.java
@@ -0,0 +1,24 @@
+package sunsetsatellite.catalyst;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.options.components.ToggleableOptionComponent;
+import net.minecraft.client.gui.options.data.OptionsPage;
+import net.minecraft.client.gui.options.data.OptionsPages;
+import sunsetsatellite.catalyst.effects.interfaces.mixins.IKeybinds;
+import turniplabs.halplibe.util.ClientStartEntrypoint;
+
+public class CatalystEffectsClient implements ClientStartEntrypoint {
+	@Override
+	public void beforeClientStart() {
+
+	}
+
+	@Override
+	public void afterClientStart() {
+		CatalystEffects.keybinds = ((IKeybinds) Minecraft.getMinecraft(Minecraft.class).gameSettings);
+		OptionsPage optionsPage = new OptionsPage("gui.options.page.catalyst-effect");
+		optionsPage
+			.withComponent(new ToggleableOptionComponent<>(CatalystEffects.keybinds.getEffectDisplayPlaceEnumOption()));
+		OptionsPages.register(optionsPage);
+	}
+}
