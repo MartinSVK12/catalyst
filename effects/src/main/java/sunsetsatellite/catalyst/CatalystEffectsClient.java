a885fc3568cd58357ba62c2f9e88e15645682705
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2024-03-24 00:47:19 +0100

    Updated to BTA 7.1-pre2a!

--- effects/src/main/java/sunsetsatellite/catalyst/CatalystEffectsClient.java
+++ effects/src/main/java/sunsetsatellite/catalyst/CatalystEffectsClient.java
@@ -4,6 +4,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.options.components.ToggleableOptionComponent;
 import net.minecraft.client.gui.options.data.OptionsPage;
 import net.minecraft.client.gui.options.data.OptionsPages;
+import net.minecraft.core.item.Item;
 import sunsetsatellite.catalyst.effects.interfaces.mixins.IKeybinds;
 import turniplabs.halplibe.util.ClientStartEntrypoint;
 
@@ -16,7 +17,7 @@ public class CatalystEffectsClient implements ClientStartEntrypoint {
 	@Override
 	public void afterClientStart() {
 		CatalystEffects.keybinds = ((IKeybinds) Minecraft.getMinecraft(Minecraft.class).gameSettings);
-		OptionsPage optionsPage = new OptionsPage("gui.options.page.catalyst-effect");
+		OptionsPage optionsPage = new OptionsPage("gui.options.page.catalyst-effect", Item.bucketWater.getDefaultStack());
 		optionsPage
 			.withComponent(new ToggleableOptionComponent<>(CatalystEffects.keybinds.getEffectDisplayPlaceEnumOption()));
 		OptionsPages.register(optionsPage);
