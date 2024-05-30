7ef46861a7cd9cff72a986fb14b50528ba193818
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2024-05-30 19:32:48 +0200

    Removed obsolete mixin. Re-added version checking.

--- fluids/src/main/java/sunsetsatellite/catalyst/CatalystFluids.java
+++ fluids/src/main/java/sunsetsatellite/catalyst/CatalystFluids.java
@@ -13,11 +13,12 @@ import sunsetsatellite.catalyst.fluids.mp.packets.PacketUpdateClientFluidRender;
 import sunsetsatellite.catalyst.fluids.registry.FluidRegistry;
 import sunsetsatellite.catalyst.fluids.registry.FluidRegistryEntry;
 import turniplabs.halplibe.helper.NetworkHelper;
+import turniplabs.halplibe.util.GameStartEntrypoint;
 
 import java.util.Collections;
 
 
-public class CatalystFluids implements ModInitializer {
+public class CatalystFluids implements ModInitializer, GameStartEntrypoint {
     public static final String MOD_ID = "catalyst-fluids";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
@@ -27,25 +28,12 @@ public class CatalystFluids implements ModInitializer {
 		NetworkHelper.register(PacketSetFluidSlot.class, true, true);
 		NetworkHelper.register(PacketFluidWindowClick.class, true, true);
 		NetworkHelper.register(PacketUpdateClientFluidRender.class, false, true);
-
 	}
 
     @Override
     public void onInitialize() {
         LOGGER.info("Catalyst: Fluids initialized.");
     }
-
-	public static void onPreLaunch() {
-		FluidRegistryEntry entry = new FluidRegistryEntry("minecraft", Item.bucketWater,Item.bucket, Collections.singletonList((BlockFluid) Block.fluidWaterFlowing));
-		FLUIDS.register("minecraft:water",entry);
-		entry = new FluidRegistryEntry("minecraft", Item.bucketLava,Item.bucket, Collections.singletonList((BlockFluid) Block.fluidLavaFlowing));
-		FLUIDS.register("minecraft:lava",entry);
-		Registries.getInstance().register(MOD_ID+":fluids",FLUIDS);
-		LOGGER.info("Fluid registry registered.");
-		LOGGER.info(FLUIDS.size()+" fluids registered.");
-	}
-
-
 
 	public static double map(double valueCoord1,
 							 double startCoord1, double endCoord1,
@@ -58,5 +46,21 @@ public class CatalystFluids implements ModInitializer {
 
 		double ratio = (endCoord2 - startCoord2) / (endCoord1 - startCoord1);
 		return ratio * (valueCoord1 - startCoord1) + startCoord2;
+	}
+
+	@Override
+	public void beforeGameStart() {
+
+	}
+
+	@Override
+	public void afterGameStart() {
+		FluidRegistryEntry entry = new FluidRegistryEntry("minecraft", Item.bucketWater,Item.bucket, Collections.singletonList((BlockFluid) Block.fluidWaterFlowing));
+		FLUIDS.register("minecraft:water",entry);
+		entry = new FluidRegistryEntry("minecraft", Item.bucketLava,Item.bucket, Collections.singletonList((BlockFluid) Block.fluidLavaFlowing));
+		FLUIDS.register("minecraft:lava",entry);
+		Registries.getInstance().register(MOD_ID+":fluids",FLUIDS);
+		LOGGER.info("Fluid registry registered.");
+		LOGGER.info(FLUIDS.size()+" fluid containers registered.");
 	}
 }
