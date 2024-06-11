23e6d627ca275ae398a1ec4a621c3effad2cf6c9
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2024-06-11 14:17:39 +0200

    Blank strings or null will not cause a new line when using ICustomDescription#getDescription.
    Added ICustomDescription#getPersistentDescription.

--- src/main/java/sunsetsatellite/catalyst/core/mixin/GuiTooltipMixin.java
+++ src/main/java/sunsetsatellite/catalyst/core/mixin/GuiTooltipMixin.java
@@ -1,5 +1,6 @@
 package sunsetsatellite.catalyst.core.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiTooltip;
 import net.minecraft.core.block.Block;
@@ -7,13 +8,22 @@ import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.item.block.ItemBlock;
 import net.minecraft.core.lang.I18n;
 import net.minecraft.core.player.inventory.slot.Slot;
+import org.jetbrains.annotations.ApiStatus;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Desc;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import sunsetsatellite.catalyst.core.util.ICustomDescription;
 
+import java.util.Objects;
+
+@Debug(
+	export = true
+)
 @Mixin(
         value = GuiTooltip.class,
         remap = false
@@ -28,12 +38,36 @@ public class GuiTooltipMixin extends Gui {
 		if(itemStack != null && itemStack.getItem() instanceof ItemBlock){
             Block block = Block.blocksList[itemStack.getItem().id];
             if(block instanceof ICustomDescription){
-                text.append(((ICustomDescription) block).getDescription(itemStack)).append("\n");
+				if(!Objects.equals(((ICustomDescription) block).getDescription(itemStack), "")){
+					text.append(((ICustomDescription) block).getDescription(itemStack)).append("\n");
+				}
             }
         }
         if(itemStack != null && itemStack.getItem() instanceof ICustomDescription){
-            text.append(((ICustomDescription) itemStack.getItem()).getDescription(itemStack)).append("\n");
+			if(!Objects.equals(((ICustomDescription) itemStack.getItem()).getDescription(itemStack), "")){
+				text.append(((ICustomDescription) itemStack.getItem()).getDescription(itemStack)).append("\n");
+			}
         }
     }
+
+	@Inject(
+		method = "getTooltipText(Lnet/minecraft/core/item/ItemStack;ZLnet/minecraft/core/player/inventory/slot/Slot;)Ljava/lang/String;",
+		at = @At(value = "JUMP",opcode = Opcodes.IFEQ,ordinal = 12)
+	)
+	public void injectPersistentTooltip(ItemStack itemStack, boolean showDescription, Slot slot, CallbackInfoReturnable<String> cir, @Local StringBuilder text){
+		if(itemStack != null && itemStack.getItem() instanceof ItemBlock){
+			Block block = Block.blocksList[itemStack.getItem().id];
+			if(block instanceof ICustomDescription){
+				if(!Objects.equals(((ICustomDescription) block).getPersistentDescription(itemStack), "")){
+					text.append("\n").append(((ICustomDescription) block).getPersistentDescription(itemStack));
+				}
+			}
+		}
+		if(itemStack != null && itemStack.getItem() instanceof ICustomDescription){
+			if(!Objects.equals(((ICustomDescription) itemStack.getItem()).getPersistentDescription(itemStack), "")){
+				text.append("\n").append(((ICustomDescription) itemStack.getItem()).getPersistentDescription(itemStack));
+			}
+		}
+	}
 }
 
--- src/main/java/sunsetsatellite/catalyst/core/util/ICustomDescription.java
+++ src/main/java/sunsetsatellite/catalyst/core/util/ICustomDescription.java
@@ -2,8 +2,13 @@ package sunsetsatellite.catalyst.core.util;
 
 
 import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.net.command.TextFormatting;
 
 public interface ICustomDescription {
 
     String getDescription(ItemStack stack);
+
+	default String getPersistentDescription(ItemStack stack) {
+		return "";
+	};
 }
