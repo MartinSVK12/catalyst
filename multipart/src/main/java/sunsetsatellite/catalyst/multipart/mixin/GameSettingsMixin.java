d058a067879131316c5dc53e0691b93471c6ab0e
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2025-01-15 15:58:51 +0100

    Add option to show/hide multiparts from TMB.

---
+++ multipart/src/main/java/sunsetsatellite/catalyst/multipart/mixin/GameSettingsMixin.java
@@ -0,0 +1,27 @@
+package sunsetsatellite.catalyst.multipart.mixin;
+
+
+import net.minecraft.client.option.BooleanOption;
+import net.minecraft.client.option.GameSettings;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import sunsetsatellite.catalyst.multipart.interfaces.mixins.IKeybinds;
+
+@Mixin(
+        value = GameSettings.class,
+        remap = false
+)
+public class GameSettingsMixin
+    implements IKeybinds
+{
+	private final GameSettings thisAs = ((GameSettings)(Object)this);
+
+	@Unique
+	BooleanOption showMultipartsInTMB = new BooleanOption(thisAs,"catalyst-multipart.showMultipartsInTMB",false);
+
+	@Override
+	public BooleanOption showMultipartsInTMB() {
+		return showMultipartsInTMB;
+	}
+
+}
