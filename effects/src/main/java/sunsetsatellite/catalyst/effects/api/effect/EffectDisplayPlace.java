c1ccdd810a53150901521f5ebbfcdd7d56e30dc8
Oly <33404595+Olypolyu@users.noreply.github.com>
2025-10-20 20:19:32 -0300

    add extra hearts from BWA

--- effects/src/main/java/sunsetsatellite/catalyst/effects/api/effect/EffectDisplayPlace.java
+++
@@ -1,7 +0,0 @@
-package sunsetsatellite.catalyst.effects.api.effect;
-
-public enum EffectDisplayPlace {
-	INVENTORY,
-	HUD,
-	BOTH
-}
---
+++ effects/src/main/java/sunsetsatellite/catalyst/effects/api/effect/render/heartContainer/HeartContainerSimple.java
@@ -0,0 +1,18 @@
+package sunsetsatellite.catalyst.effects.api.effect.render.heartContainer;
+
+import net.minecraft.core.entity.player.Player;
+
+public class HeartContainerSimple extends HeartContainer {
+
+    private final String path;
+
+    public HeartContainerSimple(Player player, String path) {
+        super(player);
+        this.path = path;
+    }
+
+    @Override
+    public String getBasePath() {
+        return path;
+    }
+}
