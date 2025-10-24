4c46a5058482bc4411f6b208efb327023069e301
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2025-10-24 11:58:07 +0200

    Small fix.

--- effects/src/main/java/sunsetsatellite/catalyst/CatalystEffects.java
+++ effects/src/main/java/sunsetsatellite/catalyst/CatalystEffects.java
@@ -1,14 +1,20 @@
 package sunsetsatellite.catalyst;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.render.texture.stitcher.TextureRegistry;
 import net.minecraft.core.data.registry.Registries;
+import net.minecraft.core.item.Items;
 import net.minecraft.core.net.command.CommandManager;
 import net.minecraft.core.net.command.util.CommandHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sunsetsatellite.catalyst.effects.api.attribute.Attributes;
+import sunsetsatellite.catalyst.effects.api.effect.Effect;
 import sunsetsatellite.catalyst.effects.api.effect.EffectTags;
 import sunsetsatellite.catalyst.effects.api.effect.Effects;
+import sunsetsatellite.catalyst.effects.api.effect.render.EffectRenderer;
+import sunsetsatellite.catalyst.effects.api.effect.render.EffectRendererDispatcher;
+import sunsetsatellite.catalyst.effects.api.effect.render.TintEffectRender;
 import sunsetsatellite.catalyst.effects.command.CommandAttributes;
 import sunsetsatellite.catalyst.effects.command.CommandEffects;
 import sunsetsatellite.catalyst.effects.interfaces.mixins.IKeybinds;
@@ -42,6 +48,21 @@ public class CatalystEffects implements ModInitializer, GameStartEntrypoint {
 
 	@Override
 	public void afterGameStart() {
+
+		EffectRendererDispatcher.getInstance().addDispatch(
+			Effects.DURATION_BOOST,
+			new TintEffectRender<>(Effects.DURATION_BOOST, null, 0x30aaff00)
+				.setIcon(TextureRegistry.getTexture("minecraft:item/diamond"))
+				.setColor(0xFFAAFF00)
+		);
+
+		EffectRendererDispatcher.getInstance().addDispatch(
+			Effects.EXTRA_HEALTH,
+			new EffectRenderer<Effect>(Effects.EXTRA_HEALTH)
+			{ @Override public boolean shouldDisplayIcon() { return false; } }
+				.setIcon(TextureRegistry.getTexture(Items.FOOD_APPLE.namespaceID))
+		);
+
 		Registries.getInstance().register("catalyst:effects",Effects.getInstance());
 		Registries.getInstance().register("catalyst:attributes",Attributes.getInstance());
 		LOGGER.info(String.format("%d attributes registered.",Attributes.getInstance().size()));
--- effects/src/main/java/sunsetsatellite/catalyst/effects/api/effect/render/EffectRendererDispatcher.java
+++ effects/src/main/java/sunsetsatellite/catalyst/effects/api/effect/render/EffectRendererDispatcher.java
@@ -27,22 +27,6 @@ public class EffectRendererDispatcher extends Dispatcher<Effect, EffectRenderer<
 		return effect == null ? getDefault() : super.getDispatch(effect);
 	}
 
-	private EffectRendererDispatcher() {
-		this.addDispatch(
-			Effects.DURATION_BOOST,
-			new TintEffectRender<>(Effects.DURATION_BOOST, null, 0x30aaff00)
-				.setIcon(TextureRegistry.getTexture("minecraft:item/diamond"))
-				.setColor(0xFFAAFF00)
-		);
-
-		this.addDispatch(
-			Effects.EXTRA_HEALTH,
-			new EffectRenderer<Effect>(Effects.EXTRA_HEALTH)
-				{ @Override public boolean shouldDisplayIcon() { return false; } }
-				.setIcon(TextureRegistry.getTexture(Items.FOOD_APPLE.namespaceID))
-		);
-	}
-
 	public static EffectRenderer<?> getRendererFor(Effect effect) {
 		return INSTANCE.getDispatch(effect);
 	}
