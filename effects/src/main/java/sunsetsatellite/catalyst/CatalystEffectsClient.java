45ed35218ab152b3c6fbd95d122703308678c42a
Oly <33404595+Olypolyu@users.noreply.github.com>
2025-10-27 16:18:33 -0300

    fix server crash

--- effects/src/main/java/sunsetsatellite/catalyst/CatalystEffectsClient.java
+++ effects/src/main/java/sunsetsatellite/catalyst/CatalystEffectsClient.java
@@ -1,8 +1,16 @@
 package sunsetsatellite.catalyst;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.options.components.ToggleableOptionComponent;
+import net.minecraft.client.render.texture.stitcher.TextureRegistry;
+import net.minecraft.core.item.Items;
 import net.minecraft.core.net.command.CommandManager;
+import sunsetsatellite.catalyst.effects.api.effect.Effect;
+import sunsetsatellite.catalyst.effects.api.effect.Effects;
+import sunsetsatellite.catalyst.effects.api.effect.render.EffectRenderer;
+import sunsetsatellite.catalyst.effects.api.effect.render.EffectRendererDispatcher;
+import sunsetsatellite.catalyst.effects.api.effect.render.TintEffectRender;
 import sunsetsatellite.catalyst.effects.command.CommandAttributes;
 import sunsetsatellite.catalyst.effects.command.CommandEffects;
 import sunsetsatellite.catalyst.effects.command.CommandExtraHealth;
@@ -22,7 +30,19 @@ public class CatalystEffectsClient implements ClientStartEntrypoint {
 
 	@Override
 	public void afterClientStart() {
+		EffectRendererDispatcher.getInstance().addDispatch(
+			Effects.DURATION_BOOST,
+			new TintEffectRender<>(Effects.DURATION_BOOST, null, 0x30aaff00)
+				.setIcon(TextureRegistry.getTexture("minecraft:item/diamond"))
+				.setColor(0xFFAAFF00)
+		);
 
+		EffectRendererDispatcher.getInstance().addDispatch(
+			Effects.EXTRA_HEALTH,
+			new EffectRenderer<Effect>(Effects.EXTRA_HEALTH)
+			{ @Override public boolean shouldDisplayIcon() { return false; } }
+				.setIcon(TextureRegistry.getTexture(Items.FOOD_APPLE.namespaceID))
+		);
 	}
 
 	public static void addSettingsPage(){
